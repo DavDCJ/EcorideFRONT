72f8eff59b93c9c20526819dd843ba5e62118fbb
DavDCJ <davinaDCJ@gmail.com>
2025-04-08 19:14:57 +0200

    page connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,8 +2,9 @@ import Route from "./route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Covoiturages", "Covoiturages", "/pages/covoiturages.html"),
+    new Route("/home", "Accueil", "/pages/home.html"),
+    new Route("/covoiturages", "Covoiturages", "/pages/covoiturages.html"),
+    new Route("/connexion", "Connexion", "/pages/connexion.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
