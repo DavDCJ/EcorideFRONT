320cf5f1522f0dda1b8cf07901a3ecac5f95ff9b
DavDCJ <davinaDCJ@gmail.com>
2025-04-09 18:08:11 +0200

    modif + inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,7 @@ export const allRoutes = [
     new Route("/home", "Accueil", "/pages/home.html"),
     new Route("/covoiturages", "Covoiturages", "/pages/covoiturages.html"),
     new Route("/connexion", "Connexion", "/pages/connexion.html"),
+    new Route("/inscription", "Inscription", "/pages/inscription.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
