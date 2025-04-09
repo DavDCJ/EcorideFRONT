c0124a183ef4091204174a1ca36e32afb96adda2
DavDCJ <davinaDCJ@gmail.com>
2025-04-09 20:09:24 +0200

    modif et page mdp

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,8 @@ export const allRoutes = [
     new Route("/covoiturages", "Covoiturages", "/pages/covoiturages.html"),
     new Route("/connexion", "Connexion", "/pages/connexion.html"),
     new Route("/inscription", "Inscription", "/pages/inscription.html"),
+    new Route("/moncompte", "Mon Compte", "/pages/moncompte.html"),
+    new Route("/editPassword", "Modifier le Mot de Passe", "/pages/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
