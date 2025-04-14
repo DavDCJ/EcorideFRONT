805f36ec1e7f3b6c37f9e4c52a4be997fe781cae
DavDCJ <davinaDCJ@gmail.com>
2025-04-14 17:58:20 +0200

    ajout fonct inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,7 @@ export const allRoutes = [
     new Route("/home", "Accueil", "/pages/home.html"),
     new Route("/covoiturages", "Covoiturages", "/pages/covoiturages.html"),
     new Route("/connexion", "Connexion", "/pages/connexion.html"),
-    new Route("/inscription", "Inscription", "/pages/inscription.html"),
+    new Route("/inscription", "Inscription", "/pages/inscription.html", "/js/auth/inscription.js"),
     new Route("/moncompte", "Mon Compte", "/pages/moncompte.html"),
     new Route("/editPassword", "Modifier le Mot de Passe", "/pages/editPassword.html"),
     new Route("/mestrajets", "Mes trajets", "/pages/mestrajets.html"),
